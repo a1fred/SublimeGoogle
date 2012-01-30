c96ae0f33ed82b129e244156c5ca0d8173b57c40
a1fred <demalf@gmail.com>
2012-01-30 16:25:49 +0400

    * rename Default.sublime-comands to right name * search user's input added

--- SublimeGoogle.py
+++ SublimeGoogle.py
@@ -10,21 +10,34 @@ import webbrowser
 import search
 
 class GoogleSearchSelectionCommand(sublime_plugin.WindowCommand):
+	"""
+	Search selected text
+	"""
+	def google_search(self,text):
+		"""
+		Search text in google and place results in self.results, then show in quick panel
+		"""
+		sublime.status_message("Searching \""+text+"\" in google.")
+		res = search.GoogleSearch(text)
+		res.results_per_page=10
+		self.results.append(["Open in browser", "http://google.com/search?q="+text])
+		for x in res.get_results():
+			self.results.append([x.title+": "+x.url, x.desc])
+		sublime.status_message("Found "+str(res.num_results)+" results.")
+
+		self.window.show_quick_panel(self.results, self.onResSelected)
+		return 0
 	def run(self):
 		v = self.window.active_view()
 		text = v.substr(v.sel()[0]).strip()
 		self.results=list()
-		sublime.status_message("Searching \""+text+"\" in google.")
-		res = search.GoogleSearch(text)
-		res.results_per_page=10
-		self.results.append(["Open in browser", "http://google.com/search?as_q="+text])
-		for x in res.get_results():
-			self.results.append([x.title+": "+x.url, x.desc])
-		self.window.show_quick_panel(self.results, self.onResSelected)
-		sublime.status_message("Found "+str(res.num_results())+"results.")
+		self.google_search(text)
+		self.show_results()
 		return 0
-
 	def onResSelected(self, num):
+		"""
+		Open page in browser
+		"""
 		if num == -1:
 			return
 		else:
@@ -32,3 +45,13 @@ class GoogleSearchSelectionCommand(sublime_plugin.WindowCommand):
 			sublime.status_message("I\'ve opened web browser for this page.")
 			webbrowser.open(url)
 			return 1
+
+class GoogleSearchInputCommand(GoogleSearchSelectionCommand):
+	"""
+	Search user's input
+	"""
+	def run(self):
+		self.results=list()
+		self.window.show_input_panel("Google search:", '', self.google_search, None, None)
+		self.show_results()
+		return 0
